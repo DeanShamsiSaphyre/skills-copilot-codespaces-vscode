d3d5bdf9ca06446f02747f2481f3640e3479fb5c
DeanShamsiSaphyre <159573858+DeanShamsiSaphyre@users.noreply.github.com>
2024-05-22 20:46:52 +0000

    adding member

---
+++ member.js
@@ -0,0 +1,4 @@
+function skillsMember() {
+  // This function will be called when the user clicks on the "Skills" button in the member view.
+  // This function will display the skills of the member.
+}
